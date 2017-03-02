e6e8dad34f47deec69913b2d3e60f04705f59945
Raju Rana <raju.rana@appsbee.com>
2017-03-02 18:36:36 +0530

    admin panel added

--- admin.php
+++ admin.php
@@ -29,7 +29,7 @@
     <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
         <div class="modal-dialog">
         <div class="loginmodal-container">
-          <h1>Login to show answers</h1><br>
+          <h1>Login to see answers</h1><br>
           <form method="post" action="showanswers.php">
             <input type="text" name="user" placeholder="Username">
             <input type="password" name="pass" placeholder="Password">
