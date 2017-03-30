ec7d39857d79479635cdbb308777a685422d9ca4
Raju Rana <raju.rana@appsbee.com>
2017-03-30 17:16:55 +0530

    admin panel change

--- db/dbcon.php
+++ db/dbcon.php
@@ -1,14 +1,14 @@
 <?php
   session_start();
-  $dbhost = '192.168.0.1';
+  /*$dbhost = '192.168.0.1';
   $dbuser = 'root';
   $dbpass = 'redhat456';
-  $dbname = 'easy_appo';
+  $dbname = 'easy_appo';*/
 
-  /*$dbhost = 'localhost';
+  $dbhost = 'localhost';
   $dbuser = 'healthon_hompro';
   $dbpass = 'T{RM=Mc^8Fqg';
-  $dbname = 'healthon_mobiledbpro';*/
+  $dbname = 'healthon_mobiledbpro';
 
   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
   $db = mysql_select_db($dbname,$conn);
